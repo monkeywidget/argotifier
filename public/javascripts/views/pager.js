ba1cebec02dbed584049da5bf1b949a4ea57b15b
MonkeyWidget <github@ghosthouse.com>
2013-06-04 21:02:35 -0700

    working jquery drawer for Control Panel

---
+++ public/javascripts/views/pager.js
@@ -0,0 +1,10 @@
+// import jquery
+
+$(document).ready(function() {
+    // DEBUG: $('#header').fadeOut('slow');
+
+    $('#panel_toggle').click( function() {
+        $('#control_panel').slideToggle('fast');
+    })
+
+});
