acb6ba8411d6ea503565be01c27087428f193742
MonkeyWidget <github@ghosthouse.com>
2013-06-04 22:09:26 -0700

    inline Control Panel, made widgets for CP

--- public/javascripts/views/pager.js
+++ public/javascripts/views/pager.js
@@ -3,6 +3,7 @@
 $(document).ready(function() {
     // DEBUG: $('#header').fadeOut('slow');
 
+    // open and close the control panel
     $('#panel_toggle').click( function() {
         $('#control_panel').slideToggle('fast');
     })
