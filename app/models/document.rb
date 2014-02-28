69ac1e68fb079687e3f1c5b9b53e665091ad1f60
Monkey Widget <github@ghosthouse.com>
2014-02-27 17:18:37 -0800

    CLI document in process - has a problem saving document

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,21 @@
+# these requires are entirely for add_new_paragraph
+require 'paragraph'
+require 'sentence'
+require 'word'
+require 'sentenceword'
+
 class Document < ActiveRecord::Base
 
   has_many :paragraphs, :order => 'paragraphs.document_index'
+
+  def add_new_paragraph( paragraph_text )
+
+    # 0-indexed
+    next_paragraph_index = paragraphs.length
+
+    paragraph = Paragraph.create!(:document => self,
+                                  :document_index => next_paragraph_index)
+    paragraph.tokenize(paragraph_text)
+  end
 
 end
