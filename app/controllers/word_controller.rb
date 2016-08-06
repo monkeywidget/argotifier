ee24f378476af5c4b2907f2bfeee3f2abcc51028
Monkey Widget <monkeywidget@users.noreply.github.com>
2016-08-05 21:02:28 -0700

    word controller

---
+++ app/controllers/word_controller.rb
@@ -0,0 +1,19 @@
+class WordController < ApplicationController
+  def count
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+# Routes, in order:
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # this has to go ABOVE the default!
+  match 'words/count' => 'words#count', :via => :get
+  match 'translated_words/by_translation/:translation' => 'translated_words#show_by_translation', :via => :get
+  match 'translated_words/by_original_word/:wordtext' => 'translated_words#show_by_original_word', :via => :get
+
+  # default: does the other RESTful routes
+  resources :words
+  # resources :translated_words
+  # resources :sentences
+  # resources :paragraphs
+  # resources :documents
 end
